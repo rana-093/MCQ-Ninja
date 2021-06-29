ab7fb4f1b215c3e3693813d038054ed99ee879f8
Masud Rana <masud.rana@masud.rana>
2021-06-29 11:27:09 +0600

    Added option List in the question

--- src/main/java/net/therap/config/JpaConfig.java
+++ src/main/java/net/therap/config/JpaConfig.java
@@ -61,7 +61,7 @@ public class JpaConfig {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
